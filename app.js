3d48fe83d20977a3a14d2e90dc57b48a53b9061d
hasanhut <hasanhut34@gmail.com>
2021-12-13 16:54:25 +0300

    commit

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+//.env MONGO_URI
+require("dotenv").config();
+//db connection
+const connectDB = require("./db/connect");
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/hello", (req, res) => {
+    res.send("Task Manager APP");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+const connect = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log("server is listening on port " + port));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+connect();
